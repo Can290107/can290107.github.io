f11fd471a574b34ea416f162ddb77cd83befa02e
Can <c.karanlik29@gmx.de>
2026-03-15 11:37:22 +0100

    counter

--- js/script.js
+++ js/script.js
@@ -123,7 +123,7 @@ const relationshipEl = document.getElementById("daysRelationship");
 
 if(togetherEl && relationshipEl){
 
-togetherEl.textContent = diffStart + " Tage kennen ";
+togetherEl.textContent = diffStart + " Tage kennen";
 relationshipEl.textContent = diffRelationship + " Tage zusammen ❤️";
 
 }
